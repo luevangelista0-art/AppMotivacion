bf1b54b7a4ffe5b15759aba759051b2b627a2965
2Bmodular <luevangelista0@gmail.com>
2025-10-14 22:31:13 -0300

    Aplicando programaçao

--- src/main/java/com/example/appmotivacion/UserActivity.kt
+++ src/main/java/com/example/appmotivacion/UserActivity.kt
@@ -1,6 +1,10 @@
 package com.example.appmotivacion
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,18 @@ class UserActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var buttonSalvar = findViewById<Button>(R.id.button_salvar)
+
+        buttonSalvar.setOnClickListener{
+            var nomeText = findViewById<EditText>(R.id.edit_text_nome).text
+            if(nomeText.isEmpty()){
+            Toast.makeText(this,"Digite o seu nome",Toast.LENGTH_LONG).show()
+        }else{
+            var intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
+
 }
