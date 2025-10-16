918789fcffedfad6cc178e02f382bfe9d7085fa0
2Bmodular <luevangelista0@gmail.com>
2025-10-16 20:24:04 -0300

    Configurando o MainActivity

--- src/main/java/com/example/appmotivacion/MainActivity.kt
+++ src/main/java/com/example/appmotivacion/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appmotivacion
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var nome = findViewById<TextView>(R.id.text_view_name)
+        var nomeRecuperacao = this.getSharedPreferences("Motivacao", MODE_PRIVATE).getString("nome","Olá Kotlin!")
+        nome.text = nomeRecuperacao
     }
 }
--- src/main/java/com/example/appmotivacion/UserActivity.kt
+++ src/main/java/com/example/appmotivacion/UserActivity.kt
@@ -28,6 +28,7 @@ class UserActivity : AppCompatActivity() {
             if(nomeText.isEmpty()){
             Toast.makeText(this,"Digite o seu nome",Toast.LENGTH_LONG).show()
         }else{
+            this.getSharedPreferences("Motivacao", MODE_PRIVATE).edit().putString("nome", nomeText.toString()).apply()
             var intent = Intent(this,MainActivity::class.java)
                 startActivity(intent)
             }
